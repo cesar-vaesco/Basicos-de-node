b1efc7410d0e047c0a144bbfc9912432d8676d89
VaEscoCesar <thecesarvargas@gmail.com>
2020-07-25 23:22:02 -0500

    node: creando un servidor y guardarlo en una función

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const http = require('http');
 
-http.createServer((req, res) => {
+const handlerServer = function(req, res) {
 
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<h1>Hola mundo desde Nodejs - Cesar </h1>');
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.write('Esto es un simple texto');
     res.end();
-}).listen(3000);
+};
+
+http.createServer(handlerServer).listen(3000);
