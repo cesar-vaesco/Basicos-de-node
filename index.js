5bcc904431003e160b8d8a8f8129c989846c2bce
VaEscoCesar <thecesarvargas@gmail.com>
2020-07-25 23:17:07 -0500

    node: creador básico de un servidor

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-const fs = require('fs');
+const http = require('http');
 
+http.createServer((req, res) => {
 
-// fs.readFile('./texto.txt', function(err, data) {
-//     if (err) {
-//         console.log(err)
-//     }
-
-//     console.log(data)
-// });
-
-fs.readFile('./texto.txt', (err, data) => {
-    if (err) {
-        console.log(err)
-    }
-
-    console.log(data.toString());
-})
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>Hola mundo desde Nodejs - Cesar </h1>');
+    res.end();
+}).listen(3000);
