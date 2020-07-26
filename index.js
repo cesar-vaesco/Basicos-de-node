a7e29cbab79f6cb4a7a300019ef5cd03afc5ce02
VaEscoCesar <thecesarvargas@gmail.com>
2020-07-26 00:22:18 -0500

    node: usando framework express y creando servidor

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
-const http = require('http');
-
+const express = require('express');
 const colors = require('colors');
 
-const handlerServer = (req, res) => {
+const server = express();
 
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<h1>Hola mundo</h1>');
-    res.end();
-};
-
-
-const server = http.createServer(handlerServer);
-
+server.get('/', (req, res) => {
+    res.send('<h1>Hola mundo con express</h1>');
+})
 server.listen(3000, () => {
-    console.log('Server en el puerto 3000'.underline.green);
+    console.log('Server on port 3000'.green);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const http = require('http');
+
+// const colors = require('colors');
+
+// const handlerServer = (req, res) => {
+
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.write('<h1>Hola mundo</h1>');
+//     res.end();
+// };
+
+
+// const server = http.createServer(handlerServer);
+
+// server.listen(3000, () => {
+//     console.log('Server en el puerto 3000'.underline.green);
+// });
