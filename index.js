9ee8a095a6b0fba39725a1d2b01424f2620936a3
VaEscoCesar <thecesarvargas@gmail.com>
2020-07-25 23:25:47 -0500

    node: creando un servidor simplificando su creación

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const http = require('http');
 
 const handlerServer = function(req, res) {
 
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.write('Esto es un simple texto');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>Hola mundo</h1>');
     res.end();
 };
 
-http.createServer(handlerServer).listen(3000);
+
+const server = http.createServer(handlerServer);
+
+server.listen(3000, function() {
+    console.log('Server en el puerto 3000');
+})
