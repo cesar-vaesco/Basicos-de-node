d49c820785c253a398d319204447f35f2f2fd4b3
VaEscoCesar <thecesarvargas@gmail.com>
2020-07-25 23:27:13 -0500

    node: servidor creado usando arrow function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const handlerServer = function(req, res) {
+const handlerServer = (req, res) => {
 
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write('<h1>Hola mundo</h1>');
@@ -10,6 +10,6 @@ const handlerServer = function(req, res) {
 
 const server = http.createServer(handlerServer);
 
-server.listen(3000, function() {
+server.listen(3000, () => {
     console.log('Server en el puerto 3000');
 })
